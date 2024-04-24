f1042a7447662099126f9e05c97de89b92d643dd
angelor13 <angelorrodrigues99@gmail.com>
2024-04-24 21:24:15 +0100

    a<szcdvfb

--- ESP32/SEXY_Robot/src/main.cpp
+++ ESP32/SEXY_Robot/src/main.cpp
@@ -5,15 +5,30 @@
 #include <Wire.h>
 //#include <DistanceGP2Y0A21YK.h>
 
-constexpr uint8_t RST_PIN = 9;     // Configurable, see typical pin layout above
-constexpr uint8_t SS_PIN = 10;     // Configurable, see typical pin layout above
 
+
+
+// RFID PINOUT
+constexpr uint8_t RST_PIN = INT8_MAX;   
+constexpr uint8_t PIN_RFID_SDA=5;
+constexpr uint8_t PIN_RFID_SCK=18;
+constexpr uint8_t PIN_RFID_MISO=19;
+constexpr uint8_t PIN_RFID_MOSI=23;
+
+MFRC522::MIFARE_Key key = {.keyByte = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}};
+
+// Lidar PINOUT
 
 static constexpr uint16_t PIN_XSHUT_FRONT = 33;
 static constexpr uint16_t ADDR_LIDAR_FRONT= 0x70;
 
 static constexpr uint32_t DIST_LIDAR_MIN = 0;
 static constexpr uint32_t DIST_LIDAR_MAX = 2600;
+
+
+
+byte data[12] = {"Embebed-Sys"};
+
 
 #define VSPI_MISO 33
 #define VSPI_MOSi 25
@@ -22,7 +37,7 @@ static constexpr uint32_t DIST_LIDAR_MAX = 2600;
 #define BUFFER_SIZE 10
 
 VL53L0X LidarFront;
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+MFRC522 mfrc522(PIN_RFID_SDA, RST_PIN);   // Create MFRC522 instance.
 
 
 
@@ -56,8 +71,114 @@ Lidar_Setup();
 
 }
 
-void loop(){
-    uint32_t distance=GetFrontDistance();
-    Serial.println("Distância medida:"+(String)distance);
 
+
+
+byte TARGET_BLOCK = 60;
+byte buffer[18];
+
+
+
+//---------------------------------- Write data to TAG ------------------------------------
+
+
+int writeBlock(int blockNumber, byte arrayAddress[]){
+  //check if the block number corresponds to data block or triler block, rtuen with error if it's trailer block.
+  int largestModulo4Number = blockNumber / 4 * 4;
+  int trailerBlock = largestModulo4Number + 3; //determine trailer block for the sector
+  if (blockNumber > 2 && (blockNumber + 1) % 4 == 0) {
+    Serial.print(blockNumber);
+    Serial.println(" is a trailer block: Error");
+    return 2;
+  }
+  //authentication
+  MFRC522::StatusCode status;
+
+  status =(MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print("Authentication failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return 3;//return "3" as error message
+  }
+  //writing data to the block
+  status = mfrc522.MIFARE_Write(blockNumber, arrayAddress, 16);
+  //status = mfrc522.MIFARE_Write(9, value1Block, 16);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print("Data write failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return 4;//return "4" as error message
+  }
 }
+
+
+
+// ---------------------------- Read data from TAG ----------------------------------- 
+
+bool readCard(byte target_block, byte read_buffer[], byte length){                     // Buffer size must be 18 bytes
+    if ( ! mfrc522.PICC_IsNewCardPresent()) { // Card present?
+        return false;
+    }
+    if ( ! mfrc522.PICC_ReadCardSerial()) {  // read UID
+   
+        return false;
+    }
+
+    MFRC522::StatusCode status;
+
+    // Authenticate using key A
+    Serial.println(F("Authenticating using key A..."));
+    status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A,target_block, &key, &(mfrc522.uid));
+    if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("PCD_Authenticate() failed: "));
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return false;
+    }
+
+    //byte length = 18;
+    status = (MFRC522::StatusCode)mfrc522.MIFARE_Read(target_block, read_buffer, &length);
+
+    if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("Tag read Failed: "));
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        return false;
+    }
+    // Halt PICC
+    mfrc522.PICC_HaltA();
+    // Stop encryption on PCD
+    mfrc522.PCD_StopCrypto1();
+    return true;
+}
+
+void loop() {
+    // Look for new cards if not found rerun the loop function
+//   if ( ! mfrc522.PICC_IsNewCardPresent()) {
+//     return;
+//   }
+//   // read from the card if not found rerun the loop function
+//   if ( ! mfrc522.PICC_ReadCardSerial())
+//   {
+//     return;
+//   }
+//   Serial.println("card detected. Writing data");
+//   writeBlock(1,data); //write data1 to the block 1 of the tag
+//   Serial.println("reading data from the tag");
+//   readCard(1, buffer,sizeof(buffer));   //read block 1
+//   //print data
+//   Serial.print("read block 1: ");
+//   for (int j = 0 ; j < 14 ; j++)
+//   {
+//     Serial.print(buffer[j]);
+//   }
+
+
+    
+    
+    if(readCard(TARGET_BLOCK, buffer, sizeof(buffer))){
+        for(int i=0; i < sizeof(buffer); i++){
+            Serial.print(buffer[i]);
+            Serial.print(" ");
+        }
+        Serial.println("");
+    }
+}
+
--- ESP32/SEXY_Robot/test/RFID.cpp
+++ ESP32/SEXY_Robot/test/RFID.cpp
@@ -1,9 +1,14 @@
 #include <SPI.h>
 #include <MFRC522.h> 
-constexpr uint8_t RST_PIN = 9;     // Configurable, see typical pin layout above
-constexpr uint8_t SS_PIN = 10;     // Configurable, see typical pin layout above
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+// RFID PINOUT
+constexpr uint8_t RST_PIN = INT8_MAX;   
+constexpr uint8_t PIN_RFID_SDA=5;
+constexpr uint8_t PIN_RFID_SCK=18;
+constexpr uint8_t PIN_RFID_MISO=19;
+constexpr uint8_t PIN_RFID_MOSI=23;
+MFRC522 mfrc522(PIN_RFID_SDA, RST_PIN);
 MFRC522::MIFARE_Key key = {.keyByte = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}};
+byte data[12] = {"Embebed-Sys"};
 
 #define N_SECTORS 15   //Sectors number
 
@@ -16,6 +21,34 @@ void setup() {
 
 byte TARGET_BLOCK = 60;
 byte buffer[18];
+
+int writeBlock(int blockNumber, byte arrayAddress[]){
+  //check if the block number corresponds to data block or triler block, rtuen with error if it's trailer block.
+  int largestModulo4Number = blockNumber / 4 * 4;
+  int trailerBlock = largestModulo4Number + 3; //determine trailer block for the sector
+  if (blockNumber > 2 && (blockNumber + 1) % 4 == 0) {
+    Serial.print(blockNumber);
+    Serial.println(" is a trailer block: Error");
+    return 2;
+  }
+  //authentication
+  MFRC522::StatusCode status;
+
+  status =(MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print("Authentication failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return 3;//return "3" as error message
+  }
+  //writing data to the block
+  status = mfrc522.MIFARE_Write(blockNumber, arrayAddress, 16);
+  //status = mfrc522.MIFARE_Write(9, value1Block, 16);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print("Data write failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return 4;//return "4" as error message
+  }
+}
 
 bool readCard(byte target_block, byte read_buffer[], byte length){                     // Buffer size must be 18 bytes
     if ( ! mfrc522.PICC_IsNewCardPresent()) { // Card present?
@@ -52,8 +85,28 @@ bool readCard(byte target_block, byte read_buffer[], byte length){              
 }
 
 void loop() {
-    byte comp=sizeof(buffer);
-    if(readCard(TARGET_BLOCK, buffer, comp)){
+        // Look for new cards if not found rerun the loop function
+//   if ( ! mfrc522.PICC_IsNewCardPresent()) {
+//     return;
+//   }
+//   // read from the card if not found rerun the loop function
+//   if ( ! mfrc522.PICC_ReadCardSerial())
+//   {
+//     return;
+//   }
+//   Serial.println("card detected. Writing data");
+//   writeBlock(1,data); //write data1 to the block 1 of the tag
+//   Serial.println("reading data from the tag");
+//   readCard(1, buffer,sizeof(buffer));   //read block 1
+//   //print data
+//   Serial.print("read block 1: ");
+//   for (int j = 0 ; j < 14 ; j++)
+//   {
+//     Serial.print(buffer[j]);
+//   }
+
+
+    if(readCard(TARGET_BLOCK, buffer, sizeof(buffer))){
         for(int i=0; i < sizeof(buffer); i++){
             Serial.print(buffer[i]);
             Serial.print(" ");
